6aaa095b60e27ac6075168a153e79debb5c89762
Jake Dykstra <jacob.d.dykstra@gmail.com>
2019-03-01 11:48:47 -0800

    removed profile scraper from profileFollowers.js

--- profileFollowers.js
+++ profileFollowers.js
@@ -78,18 +78,18 @@ Apify.main(async () => {
         console.log("end of pagination");
         await page.waitFor(3000);
         return followerRev(page);
-      }
-
-      // if scroll continues to render a new height then rescroll
-      if (height < newHeight){
-        console.log("rescroll");
-        scroller();
-      }
-      // else we are at the end of scroll, move on
-      else {
-        scrollEnd = 1;
-        scroller();
-      }
+      } else {
+          // if scroll continues to render a new height then rescroll
+          if (height < newHeight){
+            console.log("rescroll");
+            scroller();
+          }
+          // else we are at the end of scroll, move on
+          else {
+            scrollEnd = 1;
+            scroller();
+          }
+      }      
   }
 
   // gather all follower links on page
@@ -110,108 +110,8 @@ Apify.main(async () => {
         if (err) throw err;
     });
    });
-    return checkProfiles(links);
+    return;
   }
-
-  // call to loop through followers pages
-  async function checkProfiles(links) {
-    console.log(links);
-    const start = async () => {
-      await asyncForEach(links, async (val) => {
-      await page.waitFor(5000);
-      // await console.log(val);
-      await page.goto(val);
-
-      // wait for page to load before using cheerio
-         await page.waitForSelector('._2nlw', {timeout: 3000});
-         try {
-          
-      } catch (error) {
-        console.log(error);
-      }
- 
-         // gather content with cheerio
-         let content = await page.content();
-         var $ = cheerio.load(content);
-
-         // gather profile data - first, last, links, hrefs
-         var first = $('._2nlw').html().split(' ')[0];
-         var last;
-         let pageLinks = [];
-         var publicHref = [];
-
-         // get fb id
-         var initialId = $('._6-6:first-of-type').attr('href');
-         var firstCut = initialId.indexOf('%');
-         var lastCut = initialId.lastIndexOf('%');
-         var id = initialId.slice(firstCut + 3, lastCut);
-
-         // check that theirs no initials or middle name before last name
-         var nameArr = $('._2nlw').html().split(' ');
-         if (nameArr.length <= 3){
-             last = nameArr[(nameArr.length - 1)];
-         } else {
-             last = nameArr[1];
-         }
-         
-         $('._50f3 a').each((i, el) => {
-          // const info = $(el).text()
-          // if (info.includes('works at') || (info.includes('at') && !(info.includes('former')) && !(info.includes('worked')))){
-          //   // add links to pageLink array
-            if($(el).attr('href')){
-            let pageLink = $(el).attr('href');
-            console.log(pageLink);
-            pageLinks.push(pageLink);
-            //  pageLinks.push(info)
-          } 
-      
-    })
-        let betterLink = "Links: ";
-        //  const pageLinksFunc = async () => {
-        //   await asyncForEach(pageLinks, async (url) => {
-          for(let url of pageLinks){
-          await console.log(url);
-          let urlString = url.toString();
-          await page.goto(urlString);
-          await page.waitFor(5000);
-
-          // load cheerio and push hrefs to puclicHref
-          let content = await page.content();
-          var $ = cheerio.load(content);
-          if($('_v0m')){
-            console.log("class found?");
-            var jobSite = await $('._v0m a').attr('href');
-            var jobSite1 = await $('._v0m a').text();
-            await console.log(jobSite);
-            await console.log(jobSite1);
-            await publicHref.push($('._v0m a').text());
-            betterLink +=  jobSite;
-          } else if ($('._480u')) {
-            var jobSite = await $('._480u a').attr('href');
-            var jobSite1 = await $('._480u a').text();
-            await publicHref.push($('._480u a').text());
-            betterLink +=  jobSite;
-          } 
-        console.log(betterLink);
-      }
-
-         // loop through profile bullets checking for work
-
-        await page.waitFor(5000);
-        // let numOfPages = 
-        // now for each page link we will go to that site 
-        
-        console.log(`${first}, ${last}, ${id}, ${publicHref}\n\n`);
-        fs.appendFile('pageLinks_Hertado.csv', `${first}, ${last}, ${id}, ${publicHref}\n\n`, (err) => {  
-          if (err) throw err;
-          console.log('Big Write up!');
-      });
-        await page.waitFor(3000);
-    })
-    console.log('Done');
-  }
-  start();
-}
 
 console.log(`Saving new cookies to cache..`);
 const cookies = await page.cookies();
